332c5b2488f701469067ab59dd0c8abc6607a611
Jaiz <jaizepic@gmail.com>
2021-03-27 15:47:39 +0800

    Cleaned up the code. Extraction function for db.

--- main/db.py
+++ main/db.py
@@ -1,12 +1,17 @@
 import sqlite3
 
+def create_table():
+    conn = sqlite3.connect('events.db')
 
-# conn.execute('''CREATE TABLE EVENTS
-#          (ID            INT    NOT NULL,
-#          NAME           TEXT    NOT NULL,
-#          OUTDOORS       BOOLEAN     NOT NULL,
-#          BODYPART       TEXT    NOT NULL,
-#          COST           REAL    NOT NULL);''')
+    conn.execute('''CREATE TABLE EVENTS
+            (ID            INT    NOT NULL,
+            NAME           TEXT    NOT NULL,
+            OUTDOORS       BOOLEAN     NOT NULL,
+            BODYPART       TEXT    NOT NULL,
+            COST           REAL    NOT NULL);''')
+    
+    conn.commit()
+    conn.close()
 
 def input_event(event_string):
     conn = sqlite3.connect('events.db')
@@ -35,10 +40,22 @@ def database_entry(value_list):
     return f"INSERT INTO EVENTS (ID, NAME, OUTDOORS, BODYPART, COST) \
         VALUES ({value_list[0]}, '{value_list[1]}', {value_list[2]}, '{value_list[3]}', {value_list[4]} )"
 
+def database_extract():
+    conn = sqlite3.connect(":memory:")
+    conn.row_factory = sqlite3.Row
+    c = conn.cursor()
+    c.execute('select * from EVENTS')
+    L = []
+    for row in c.fetchall():
+        L.append(dict(row))
+    return L
+
+'''
 conn = sqlite3.connect('events.db')
 
-#conn.execute("DELETE from EVENTS where ID = 0;") 
-# conn.execute("INSERT INTO EVENTS (ID,NAME,OUTDOORS,BODYPART,COST) \
-#     VALUES (0, 'a', False, 'a', 0)");
+conn.execute("DELETE from EVENTS where ID = 0;") 
+conn.execute("INSERT INTO EVENTS (ID,NAME,OUTDOORS,BODYPART,COST) \
+     VALUES (0, 'a', False, 'a', 0)");
 conn.commit()
 conn.close()
+'''
